a42bfe8188094cc078e2e29da096742a7eb4f3d4
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-09-06 17:31:27 +0300

    Update to godot 4.1.1

--- src/ffb_plugin.h
+++ src/ffb_plugin.h
@@ -10,42 +10,39 @@
 
 #include "godot_cpp/classes/node.hpp"
 
-namespace godot{
-class FFBPlugin : public Node
-{
-    GDCLASS(FFBPlugin, Node)
+namespace godot {
+class FFBPlugin : public Node {
+  GDCLASS(FFBPlugin, Node)
 
-    public:
-        FFBPlugin();
-        ~FFBPlugin();
-        void _init();
-        
-		int init_ffb(int p_device);
-        int init_constant_force_effect();
-        
-		int update_constant_ffb_effect(float force, int length, int effect_id);
-        int play_constant_ffb_effect(int effect_id, int iterations);
-        
-		void destroy_ffb_effect(int effect_id);
-		void close_ffb_device();
+public:
+  FFBPlugin();
+  ~FFBPlugin();
+  void _init();
 
-		bool has_force_feedback();
-		bool has_constant_force();
-		
-	protected:
-        static void _bind_methods();
+  int init_ffb(int p_device);
+  int init_constant_force_effect();
 
-    private:
-        SDL_Haptic *haptic;
-        SDL_Joystick *joy;
-        SDL_HapticEffect effect;
+  int update_constant_ffb_effect(float force, int length, int effect_id);
+  int play_constant_ffb_effect(int effect_id, int iterations);
 
-		int autocenter = 0;
-		bool force_feedback = false;
+  void destroy_ffb_effect(int effect_id);
+  void close_ffb_device();
 
+  bool has_force_feedback();
+  bool has_constant_force();
 
-        bool constant_force = false;
+protected:
+  static void _bind_methods();
+
+private:
+  SDL_Haptic *haptic;
+  SDL_Joystick *joy;
+  SDL_HapticEffect effect;
+
+  int autocenter = 0;
+  bool force_feedback = false;
+
+  bool constant_force = false;
 };
-}
+} // namespace godot
 #endif // FFB_PLUGIN_H
-
